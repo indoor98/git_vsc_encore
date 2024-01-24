7886e0a7b30107d6dfb51d4abff494ce9a88e8dd
jmh <thinghoon@icloud.com>
2024-01-24 14:33:37 +0900

    ha

--- cote.py
+++ cote.py
@@ -13,7 +13,18 @@ for _ in range(T):
     rad2 = math.sqrt( (B[0] - C[0])**2 + (B[1] - C[1])**2)
     between = math.sqrt( (A[0] - B[0])**2 + (A[1] - B[1])**2)
 
+
+    print(rad1, rad2, between)
+
     if (rad1 + rad2) < between :
-        print(-1)
+        print(0)
     
     elif (rad1 + rad2) == between :
+        print(1)
+    
+    elif (rad1 + rad2) > between :
+        if (min(rad1, rad2) + between) < max(rad1, rad2) :
+            print(-1)
+        elif (min(rad1, rad2) + between) == max(rad1, rad2) :
+            print(1)
+        else: print(2)
