af556fa8dff8a36badd66724c141644a2c1aefc0
jmh <thinghoon@icloud.com>
2024-01-24 15:07:15 +0900

    sdafsdf

--- cote.py
+++ cote.py
@@ -11,20 +11,26 @@ for _ in range(T):
 
     rad1 = math.sqrt( (A[0] - C[0])**2 + (A[1] - C[1])**2)
     rad2 = math.sqrt( (B[0] - C[0])**2 + (B[1] - C[1])**2)
-    between = math.sqrt( (A[0] - B[0])**2 + (A[1] - B[1])**2)
+    bet = math.sqrt( (A[0] - B[0])**2 + (A[1] - B[1])**2)
 
+    large = max(rad1, rad2)
+    small = min(rad1, rad2)
 
-    print(rad1, rad2, between)
-
-    if (rad1 + rad2) < between :
-        print(0)
-    
-    elif (rad1 + rad2) == between :
-        print(1)
-    
-    elif (rad1 + rad2) > between :
-        if (min(rad1, rad2) + between) < max(rad1, rad2) :
+    ## 두 원의 중심이 같은 경우
+    if bet == 0:
+        if rad1 == rad2:
             print(-1)
-        elif (min(rad1, rad2) + between) == max(rad1, rad2) :
-            print(1)
-        else: print(2)
+        else:
+            print(0)
+        continue
+
+    if (rad1+rad2) < bet:
+        print(0)
+        continue
+
+    elif (rad1+rad2) == bet:
+        print(1)
+        continue
+    
+    else: # rad1 + rad2 > bet 
+    
