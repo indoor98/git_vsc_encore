1cd1ca7dcd0538eeb0cf45d3264422dd13aa1509
jmh <thinghoon@icloud.com>
2024-01-24 16:21:04 +0900

    dsfds

--- cote.py
+++ cote.py
@@ -6,31 +6,36 @@ for _ in range(T):
     position = list(map(int, sys.stdin.readline().split()))
 
     A = position[:2]
-    B = position[2:4]
-    C = position[4:]
-
-    rad1 = math.sqrt( (A[0] - C[0])**2 + (A[1] - C[1])**2)
-    rad2 = math.sqrt( (B[0] - C[0])**2 + (B[1] - C[1])**2)
-    bet = math.sqrt( (A[0] - B[0])**2 + (A[1] - B[1])**2)
-
-    large = max(rad1, rad2)
-    small = min(rad1, rad2)
+    B = position[3:5]
+    r1 = position[2]
+    r2 = position[5]
+    r3 = math.sqrt( (A[0] - B[0])**2 + (A[1] - B[1])**2)
 
     ## 두 원의 중심이 같은 경우
-    if bet == 0:
-        if rad1 == rad2:
+    if r3 == 0:
+        if r1 == r2:
             print(-1)
         else:
             print(0)
         continue
 
-    if (rad1+rad2) < bet:
-        print(0)
-        continue
+    ## 두 원의 중심이 다른 경우
 
-    elif (rad1+rad2) == bet:
-        print(1)
-        continue
-    
-    else: # rad1 + rad2 > bet 
-    
+    # 포함 관계가 아닌 경우
+    if r3 > max(r1, r2):
+        if (r1 + r2) < r3:
+            print(0)
+        elif (r1 + r2) == r3:
+            print(1)
+        else :
+            print(2)
+    elif r3 < max(r1, r2): # 한 원이 다른 원에 포함되는 경우
+        if max(r1, r2) > (r3 + min(r1, r2)) :
+            print(0)
+        elif max(r1, r2) == (r3+min(r1, r2)) :
+            print(1)
+        else:
+            print(2)
+    else:
+        print(2)
+        
