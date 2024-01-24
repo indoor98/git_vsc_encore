cd404782b00086b6ae98c37ed871349a13691d95
jmh <thinghoon@icloud.com>
2024-01-24 14:20:07 +0900

    충돌

--- cote.py
+++ cote.py
@@ -9,6 +9,11 @@ for _ in range(T):
     B = position[2:4]
     C = position[4:]
 
-    radius1 = math.sqrt( (A[0] - C[0])**2 + (A[1] - C[1])**2)
-    radius2 = 
-    print(radius1)
+    rad1 = math.sqrt( (A[0] - C[0])**2 + (A[1] - C[1])**2)
+    rad2 = math.sqrt( (B[0] - C[0])**2 + (B[1] - C[1])**2)
+    between = math.sqrt( (A[0] - B[0])**2 + (A[1] - B[1])**2)
+
+    if (rad1 + rad2) < between :
+        print(-1)
+    
+    elif (rad1 + rad2) == between :
