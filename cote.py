f8fbf837222e5dd53940a1756a9f5c5596489e8a
jmh <thinghoon@icloud.com>
2024-01-24 12:23:49 +0900

    leopards 멤버 추가

--- cote.py
+++ cote.py
@@ -1,9 +1,14 @@
-import sys
+import sys, math
 
 T = int(sys.stdin.readline())
 
 for _ in range(T):
     position = list(map(int, sys.stdin.readline().split()))
 
-    print(position)
+    A = position[:2]
+    B = position[2:4]
+    C = position[4:]
 
+    radius1 = math.sqrt( (A[0] - C[0])**2 + (A[1] - C[1])**2)
+    radius2 = 
+    print(radius1)
